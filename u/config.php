4a2632d202db8fdf061efb9063246e66b63ad507
MacBook <raze2736@gmail.com>
2018-05-06 00:17:53 -0400

    a

---
+++ u/config.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+	/* This is a secure key that only you should know, an added layer of security for the image upload */
+    'secure_key' => 'FUCKNIGGERS',
+
+    /* This is the url your output will be, usually http://www.domain.com/u/, also going to this url will be the gallery page */
+    'output_url' => 'https://voidxd.me/i/',
+
+    /* This is a redirect url if the script is accessed directly */
+    'redirect_url' => 'http://voidxd.me/',
+
+    /* This is a list of IPs that can access the gallery page (Leave empty for universal access) */
+    'allowed_ips' => array(),
+
+    /* Page title of the gallery page */
+    'page_title' => 'Void\'s thingie',
+
+    /* Heading text at the top of the gallery page */
+    'heading_text' => 'Uploading Site',
+);
