245edd6de0b0815606a55bd38183e82b97a532cc
MacBook <raze2736@gmail.com>
2018-05-06 02:30:46 -0400

    a

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 
 <html>
 
+  <head>
+    <?php
+      $bgArr = scandir("bg/");
+
+      $i = rand(0, count($bgArr)-1);
+      $bg = "$bgArr[$i]";
+    ?>
+
+    <style type="text/css">
+    body{
+      background: url(bg/<?php echo $bg; ?>) no-repeat;
+    }
+
+    </style>
+
+  </head>
+
   <body>
-  xd
+    <audio id="song" style="display:none;" src="files/H E R B - Moving.mp3" controls autoplay loop>
   </body>
 
 </html>
