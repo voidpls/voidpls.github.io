01cbaaeef04d35bd7aaa529f2b9c7da0a2154b9f
MacBook <raze2736@gmail.com>
2018-05-06 22:30:48 -0400

    done bugfixing (hopefully)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 
 function loadBG(bg) {
   console.log(bg)
-/*
-  $('<img/>').attr('src', `files/bg$/{bg}`).on('load', function() {
+
+  $('<img/>').attr('src', `files/bg/${bg}`).on('load', function() {
      $(this).remove();
-     $('body').css('background', `url(files/bg$/{bg}) no-repeat 50% 50% fixed;`);
+     $('body').css('background', `url(files/bg/${bg}) no-repeat 50% 50% fixed;`);
      var song = new Audio('../files/H E R B - Moving.mp3');
      song.play();
      song.loop = true;
   });
-*/
+
 }
