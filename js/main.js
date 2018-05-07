5b5a7564445a8001cb72a1dc3c2a5bc9fdaa4989
MacBook <raze2736@gmail.com>
2018-05-06 22:04:01 -0400

    a

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function loadBG(bg) {
   $('<img/>').attr('src', 'files/bg/${bg}').on('load', function() {
      $(this).remove();
      $('body').css('background', `url(files/bg/${bg}) no-repeat 50% 50% fixed;`);
-
+     console.log(bg)
      var song = new Audio('../files/H E R B - Moving.mp3');
      song.play();
      song.loop = true;
