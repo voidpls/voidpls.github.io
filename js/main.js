acecf2e66cfe772aef5d127312c9f63e9729b523
MacBook <raze2736@gmail.com>
2018-05-07 20:41:50 -0400

    aa

--- js/main.js
+++ js/main.js
@@ -3,15 +3,14 @@ function loadBG(bgFile) {
 
   let bgAddr = "../files/bg/"+bgFile;
 
-  $("#bg").css("background-image", "url('"+bgAddr+"')");
-//$("#bg").css("background-image", "url('../files/bg/rain1.gif')");
 
-  /*$('<img/>').attr("src", bgAddr).on('load', function() {
+  $('<img/>').attr("src", bgAddr).on('load', function() {
      $(this).remove();
-     $('body').css("background", "url(${"+bgAddr+"}) no-repeat 50% 50% fixed;");
-     var song = new Audio('../files/H E R B - Moving.mp3');
+	 $("#bg").css("background-image", "url('"+bgAddr+"')");     
+	 
+	 var song = new Audio('../files/H E R B - Moving.mp3');
      song.play();
      song.loop = true;
-  });*/
+  });
 
 }
