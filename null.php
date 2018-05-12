5a9bc466aa0c6c7eef580451808f0f3fbab394f3
MacBook <raze2736@gmail.com>
2018-05-12 13:24:13 -0400

    html => php

--- null.php
+++ null.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        
+
        <title>Null Bot</title>
        <meta property="og:title" content="Null Bot" />
        <meta property="og:type" content="website" />
@@ -10,11 +10,11 @@
        <meta property="og:description" content="Invite link for Null Bot." />
        <link rel="icon" type="image/png" href="null/favicon-32x32.png" sizes="32x32" />
        <link rel="icon" type="image/png" href="null/favicon-16x16.png" sizes="16x16" />
-  
-       <meta name="theme-color" content="#ffffff"> 
-        
+
+       <meta name="theme-color" content="#ffffff">
+
     </head>
-    <script> 
+    <script>
         window.location = "https://discordapp.com/api/oauth2/authorize?client_id=430081374444191775&permissions=0&scope=bot";
     </script>
 </html>
