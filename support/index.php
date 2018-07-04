59850a104a348ed35754d6dd956a0fac3ee4ffe8
Win10@MacBook <raze2736@gmail.com>
2018-07-04 18:19:20 -0400

    support redirect

---
+++ support/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <title>Null Support</title>
+    </head>
+    <script>
+        window.location = "https://discordapp.com/invite/BghFXRy";
+    </script>
+</html>
