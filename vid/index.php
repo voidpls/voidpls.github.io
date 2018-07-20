ba4f8aa2c4b6999dd1488af78d2510f521f9b09e
Void <raze2736@gmail.com>
2018-07-20 15:41:20 -0400

    Vid embed test

---
+++ vid/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <meta property="og:video" content="https://voidxd.me/vid/video.mp4" />
+       <meta name="theme-color" content="#36393E">
+    </head>
+    <script>
+        window.location = "https://voidxd.me/";
+    </script>
+</html>
