719d03bb50cdc2615c745858f3818fc68434db5c
Void <voidpls@protonmail.com>
2019-01-14 20:55:28 -0500

    transfer to GH pages

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
+function init(bgNum) {
+  let bgAddr = `../files/bg/rain${bgNum}.gif`
+  $('<img/>')
+    .attr('src', bgAddr)
+    .on('load', function() {
+      $(this).remove()
+      $('#bg').css('background-image', "url('" + bgAddr + "')")
 
-function loadBG(bgFile) {
-
-  let bgAddr = "../files/bg/"+bgFile;
-
-
-  $('<img/>').attr("src", bgAddr).on('load', function() {
-     $(this).remove();
-	 $("#bg").css("background-image", "url('"+bgAddr+"')");     
-	 
-	 var song = new Audio('../files/H E R B - Moving.mp3');
-     song.play();
-     song.loop = true;
-  });
-
+      let song = new Audio('files/H E R B - Moving.mp3')
+      song.play()
+      song.loop = true
+    })
 }
+
+let bgNum = ~~(Math.random() * 10) + 1
+// let songs = {
+//   moving: {
+//     name: ""
+//   }
+// }
+init(bgNum)
