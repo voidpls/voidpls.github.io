d762fe2602a80d358b96a092382a146c7c6618c5
Void <voidpls@protonmail.com>
2020-04-09 08:29:24 +0000

    Update index.php

--- vid/index.php
+++ vid/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-       <meta property="og:video" content="https://voidxd.me/vid/video.mp4" />
+       <meta property="og:video" content="https://voidpls.github.io/vid/video.mp4" />
        <meta name="theme-color" content="#36393E">
     </head>
-    <script>
+<!--     <script>
         window.location = "https://voidxd.me/";
-    </script>
+    </script> -->
 </html>
