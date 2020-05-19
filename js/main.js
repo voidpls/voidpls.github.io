23b382281727c627eb31f1106d4bdc1bdd35f36b
Void <voidpls@protonmail.com>
2020-05-19 03:33:57 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,4 +28,11 @@ $(document).ready(function() {
         duration: 400
       })
   })
+    var videoWidth = 1280;
+    var  video = $(".video-container video").attr("srcdesk");
+    if($(window).width() < 721) {
+      videoWidth = 720;
+      video = $(".video-container video").attr("srcmob"));
+    }
+    $(".video-container video").attr("width", videoWidth).append('<source src="' + video + '" type="video/mp4" >'),
 })
