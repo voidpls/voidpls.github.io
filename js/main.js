c9f1bd5440a97dc67e5d63251c7232a90f8428bf
Void <voidpls@protonmail.com>
2020-05-22 00:21:13 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
-//function init(bgNum) {
- // let bgAddr = `files/bg/rain${bgNum}.gif`
+function init(bgNum) {
+ let bgAddr = `files/bg/rain${bgNum}.gif`
 
-  //$('<img/>')
-    //.attr('src', bgAddr)
-    //.on('load', function() {
-      //$(this).remove()
-      //$('#bg').css('background-image', "url('" + bgAddr + "')")
-      //let song = new Audio('files/H E R B - Moving.mp3')
-      //song.loop = true
-      //song.volume = 0.75
-      //song.play()
-    //})
-//}
+  $('<img/>')
+    .attr('src', bgAddr)
+    .on('load', function() {
+      $(this).remove()
+      $('#bg').css('background-image', "url('" + bgAddr + "')")
+//       let song = new Audio('files/H E R B - Moving.mp3')
+//       song.loop = true
+//       song.volume = 0.75
+//       song.play()
+    })
+}
 
-//let bgNum = ~~(Math.random() * 6) + 1
-//init(bgNum)
+const bgs = [1,2,3,5,6]
+const bgNum = bgs[bgs.length]
+// let bgNum = ~~(Math.random() * 6) + 1
+init(bgNum)
 
 $(document).ready(function() {
 //   $("#bgndVideo").mb_YTPlayer();
@@ -28,13 +30,13 @@ $(document).ready(function() {
         duration: 400
     })
   })
-    var videoWidth = 1280;
-    var videoHeight = 720;
-    var video = $(".video-container video").attr("srcdesk");
-    if($(window).width() < 721) {
-      videoWidth = 480;
-      videoHeight = 854;
-      video = $(".video-container video").attr("srcmob");
-    }
-    $(".video-container video").attr("width", videoWidth).attr("height", videoHeight).append('<source src="' + video + '" type="video/mp4" >')
+//     var videoWidth = 1280;
+//     var videoHeight = 720;
+//     var video = $(".video-container video").attr("srcdesk");
+//     if($(window).width() < 721) {
+//       videoWidth = 480;
+//       videoHeight = 854;
+//       video = $(".video-container video").attr("srcmob");
+//     }
+//     $(".video-container video").attr("width", videoWidth).attr("height", videoHeight).append('<source src="' + video + '" type="video/mp4" >')
 })
